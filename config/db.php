244b1ad111c2673633ad6d8d2b6b845f6a66331b
Vignesh <vigneshadhi773@gmail.com>
2026-03-16 22:20:44 +0530

    configure secure ssl connections for tidb Updated the MySQL database connection string in
    config/db.php
     to use MYSQLI_CLIENT_SSL parameter with mysqli_real_connect() since TiDB Serverless requires encrypted transport.

--- config/db.php
+++ config/db.php
@@ -7,7 +7,9 @@ $password = "1VGO8nDShvxzrmE2";
 $database = "test";
 $port = 4000;
 
-$conn = mysqli_connect($host, $user, $password, $database, $port);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $host, $user, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
