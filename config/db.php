9af043b5c026e1a6f059c03587dc66a7c8f3157e
Vignesh <vigneshadhi773@gmail.com>
2026-03-16 22:01:40 +0530

    The DB has changed from railway to TiDB

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
 require_once __DIR__ . '/app.php';
 
-$host = "mainline.proxy.rlwy.net";
-$user = "root";
-$password = "tqwXGAjLuYJOFfXmRudQyqESSRlhRGmu";
-$database = "railway";
-$port = 32459;
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$user = "3PsojpazoRe8fKN.root";
+$password = "1VGO8nDShvxzrmE2";
+$database = "test";
+$port = 4000;
 
 $conn = mysqli_connect($host, $user, $password, $database, $port);
 
