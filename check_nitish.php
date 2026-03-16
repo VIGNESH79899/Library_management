c93a2eec24c69f847b682345efbd05bf62c1bd29
Vignesh <vigneshadhi773@gmail.com>
2026-03-16 20:16:57 +0530

    add script to sync existing librarians to admin table Created a utility script (
    sync_staff.php
    ) to automatically generate staff-role login credentials in the admin table for any pre-existing librarians.

--- check_nitish.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'c:/xampp/htdocs/Library-management/config/db.php';
-$res = $conn->query("SELECT * FROM librarian WHERE Email='nitish@library.com'");
-while ($r = $res->fetch_assoc()) print_r($r);
-?>
